56aaf3866925e820181f50e6477220a7d1d40d92
FOXX <siliconfoxx@gmail.com>
2016-10-23 22:56:01 -0500

    Updated scores

--- gloom/app/models/score.rb
+++ gloom/app/models/score.rb
@@ -1,2 +1,7 @@
 class Score < ApplicationRecord
+  # Update a Score
+  def update(x)
+    self.value = x
+    self.save!
+  end
 end
--- gloom/db/schema.rb
+++ gloom/db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20161024031726) do
   end
 
   create_table "scores", force: :cascade do |t|
-    t.string   "type"
+    t.string   "name"
     t.integer  "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
