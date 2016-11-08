b4dc00d56e77a58b4adba1cb129309a8faf2a33b
FOXX <siliconfoxx@gmail.com>
2016-11-08 00:39:01 -0600

    First pass at making the scoreboard updated over ActionCable

--- gloom/app/models/score.rb
+++ gloom/app/models/score.rb
@@ -1,4 +1,11 @@
 class Score < ApplicationRecord
+  after_update_commit :send_channel_update
+  
+  # Broadcast Challenge update
+  def send_channel_update
+    ScoreBroadcastJob.perform_later(self.id, self.value)
+  end
+
   # Update a Score
   def update(x)
     self.value = x
