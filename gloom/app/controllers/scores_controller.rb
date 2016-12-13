c59befb92374681c5108a7526fd054b40269a18b
FOXX <siliconfoxx@gmail.com>
2016-12-13 00:48:00 -0600

    Added par time and altered scoring

--- gloom/app/controllers/scores_controller.rb
+++ gloom/app/controllers/scores_controller.rb
@@ -1,6 +1,7 @@
 class ScoresController < ApplicationController
   def index
     @time       = Score.where(:name => "time").pluck(:value)[0]
+    @par_time   = Level.where(:name => "par").pluck(:value)[0]
     @kills      = Score.where(:name => "kills").pluck(:value)[0]
     @secrets    = Score.where(:name => "secrets").pluck(:value)[0]
     @challenges = Score.where(:name => "challenges").pluck(:value)[0]
