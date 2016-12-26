1a5d56e64b97911bbf0c36400626252a1454edf0
FOXX <siliconfoxx@gmail.com>
2016-12-26 15:17:12 -0600

    Updated the arganium-client.sh script with a more full-featured version

--- certain/examples/arganium-client.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-/opt/zandronum/zandronum -iwad $1 -file ~/repos/arganium/assets/arganium.pk3 ~/repos/arganium/levels/arganium01/arganium01.wad +connect localhost
--- certain/examples/arganium-server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# This assumes you are in the certain/bin directory
-./certain.rb --iwad $1 --wadfiles ~/repos/arganium/levels/arganium01/arganium01.wad --assets ~/repos/arganium/assets/arganium.pk3 --level hack01 --marines 2
