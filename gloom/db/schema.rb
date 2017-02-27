00d6795a56600edcad642dc9efcf07d1a11224c5
FOXX <siliconfoxx@gmail.com>
2017-02-26 21:56:52 -0600

    Updated schema

--- gloom/db/schema.rb
+++ gloom/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213063037) do
+ActiveRecord::Schema.define(version: 20170223064020) do
 
   create_table "challenges", force: :cascade do |t|
     t.string   "name"
@@ -37,8 +37,9 @@ ActiveRecord::Schema.define(version: 20161213063037) do
 
   create_table "hacklifts", force: :cascade do |t|
     t.string   "sector"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "direction",  default: "both"
   end
 
   create_table "levels", force: :cascade do |t|
